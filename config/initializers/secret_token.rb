2fbab428e7c4bde1962fde3045d5acd0c23d4872
Kristofer M White <me@kmwhite.net>
2013-06-19 16:13:35 -0500

    Adding rails crap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Comicdex::Application.config.secret_token = '13e00ca85fa235406df5aa98f9bf654470cb1a1fd573cb4bc4cbff2996140986d8b8c2c491c547dc84a89834da56934a03d3c4753b278e4fda6f78ae6242e8ae'
