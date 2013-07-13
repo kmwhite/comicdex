0e9674423e7a28a6bc9de8e0e900abef6d93dc15
Kristofer M White <me@kmwhite.net>
2013-07-13 15:03:03 -0500

    Semi completed Volume show -- hit /volume/show/1

--- app/models/volume.rb
+++ app/models/volume.rb
@@ -3,4 +3,19 @@ class Volume < ActiveRecord::Base
   attr_accessible :name, :number
 
   has_many :issues
+
+  def publication_start_date
+    issues.sort_by { |i| i.publication_date }.first.publication_date
+  end
+  alias_method :start_date, :publication_start_date
+
+  def publication_stop_date
+    issues.sort_by { |i| i.publication_date }.last.publication_date
+  end
+  alias_method :stop_date, :publication_stop_date
+
+  def publication_date_range(pretty_print = false)
+    pretty_print ?  start_date..stop_date :
+      "#{start_date.to_s} - #{stop_date.to_s}"
+  end
 end
