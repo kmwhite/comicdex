96f0c427bbd7aafb5663d20ad58b35828ccdba03
Kristofer M White <me@kmwhite.net>
2013-07-17 19:32:03 -0500

    Making issue titles more friendly

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -3,4 +3,8 @@ class Issue < ActiveRecord::Base
   attr_accessible :description, :number, :publication_date, :title
   attr_accessible :cover
   has_attached_file :cover, :styles => { :large => "487x750>", :medium => "325x500", :thumb => "156x240>" }, :default_url => "http://placehold.it/500x500&text=Missing%20Cover"
+
+  def series_name
+    volume.series.name
+  end
 end
