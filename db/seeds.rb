7b3649d8fae9a1b05bd579346f5c02b9eb294de8
Kristofer M White <me@kmwhite.net>
2013-07-26 14:29:19 -0500

    Cleaning up the seeds to generate a random amount of data

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,9 +12,9 @@ SeriesType.create([
 ], without_protection: true )
 
 Series.create([
-  { name: "Funny Stories", description: "Kooky Shenanigans!",   publisher_id: 1, series_type_id: 1 },
-  { name: "Scary Stories", description: "Terrifying Stories!",  publisher_id: 1, series_type_id: 3 },
-  { name: "Lovey Stories", description: "Love Stories?! Bah!",  publisher_id: 1, series_type_id: 4 }
+  { name: "Funny Stories", description: "Hilarious Hijinks!",   publisher_id: 1, series_type_id: 1 },
+  { name: "Scary Stories", description: "Terrifying Tales!",  publisher_id: 1, series_type_id: 3 },
+  { name: "Action Stories", description: "Edge-of-Your-Seat Excitement!",  publisher_id: 1, series_type_id: 4 }
 ], without_protection: true )
 
 Volume.create([
@@ -24,14 +24,43 @@ Volume.create([
   { name: "Volume I",  number: 1, series_id: 3 }
 ], without_protection: true )
 
-# Setup Covers using
-Issue.create([
-  { title: "Funny Tales",          description: "Shut up and read them", number: 1, publication_date: 'Apr 1 2012'.to_date, volume_id: 1 },
-  { title: "Funnier Tales",        description: "Shut up and read them", number: 2, publication_date: 'Apr 8 2012'.to_date, volume_id: 1 },
-  { title: "Funniest Tales",       description: "Shut up and read them", number: 1, publication_date: 'Apr 15 2012'.to_date, volume_id: 2 },
-  { title: "Funniest-er Tales",    description: "Shut up and read them", number: 2, publication_date: 'May 15 2012'.to_date, volume_id: 2 },
-  { title: "Scary Stuff",          description: "Shut up and read them", number: 1, publication_date: 'Oct 31 2012'.to_date, volume_id: 3 },
-  { title: "Horror'n'Fear",        description: "Shut up and read them", number: 2, publication_date: 'Oct 31 2013'.to_date, volume_id: 3 },
-  { title: "Histoires d'amour",    description: "Shut up and read them", number: 1, publication_date: 'Feb 14 2000'.to_date, volume_id: 4 },
-  { title: "histoires de romance", description: "Shut up and read them", number: 2, publication_date: 'Feb 14 2007'.to_date, volume_id: 4 }
-], without_protection: true )
+(1..Random.rand(50)).each do |number|
+  i = Issue.create(
+    title: "Funny Fables #{number.to_s.rjust(2, '0')}",
+    number: number,
+    description: "#{number.to_s.rjust(2, '0')}",
+    publication_date: 'Apr 1 2010'.to_date + (number*7),
+  )
+  i.volume_id = 1
+  i.save!
+end
+(1..Random.rand(50)).each do |number|
+  i = Issue.create(
+    title: "Funnier Fables #{number.to_s.rjust(2, '0')}",
+    number: number,
+    description: "#{number.to_s.rjust(2, '0')}",
+    publication_date: 'Apr 1 2011'.to_date + (number*7),
+  )
+  i.volume_id = 2
+  i.save!
+end
+(1..Random.rand(50)).each do |number|
+  i = Issue.create(
+    title: "Scary Stories#{number.to_s.rjust(2, '0')}",
+    number: number,
+    description: "#{number.to_s.rjust(2, '0')}",
+    publication_date: "Oct 31 #{1941+number}".to_date,
+  ) 
+  i.volume_id = 3
+  i.save!
+end
+(1..Random.rand(50)).each do |number|
+  i = Issue.create(
+    title: "Big Bada Boom #{number.to_s.rjust(2, '0')}",
+    number: number,
+    description: "GUNS AND EXPLOSIONS! OH YEAH * #{number.to_s.rjust(2, '0')}",
+    publication_date: 'Jul 4 2000'.to_date + (number*50) + Random.rand(365),
+  )
+  i.volume_id = 4
+  i.save!
+end
